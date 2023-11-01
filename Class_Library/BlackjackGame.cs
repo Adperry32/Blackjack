a440fc263e591572ef87a87a269ed2566bed308e
Arrington Perry <perryceo21@hotmail.com>
2023-11-01 19:57:30 -0400

    code cleanup and implementation of BlackjackGame class

--- Class_Library/BlackjackGame.cs
+++ Class_Library/BlackjackGame.cs
@@ -8,25 +8,45 @@ namespace Class_Library
 {
     public  class BlackjackGame
     {
-        BlackjackHand _dealer = new BlackjackHand();
+        BlackjackHand _dealer = new BlackjackHand(true);
         BlackjackHand _player = new BlackjackHand();    
         Deck _deck = new Deck();
 
 
         public void PlayRound()
         {
-            
+            Console.Clear();
+            DealInitialCards();
+            PlayersTurn();
+            DealersTurn();
+            DeclareWinner();
 
         }
 
         public void DealInitialCards() 
         {
+            _deck.Shuffle();
+
+            //deal two cards to player and dealer
 
         }
 
         public void PlayersTurn() 
         {
-        
+            int playerX = 30;
+            int playerY = 10;   
+            while(_player.Score < 21)
+            {
+                Console.WriteLine("Player's turn. Do you want to HIT or STAND? (Type 'H' for Hit, 'S' for Stand) ");
+                string Choice = Console.ReadLine();
+
+                if(Choice.Equals("H", StringComparison.OrdinalIgnoreCase))
+                {
+                    _deck.Deal(1);
+                    _player.Draw(playerX, playerY);
+                }
+            }
+
         }
         public void DealersTurn()
         {
--- Class_Library/Deck.cs
+++ Class_Library/Deck.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Class_Library
@@ -26,23 +27,35 @@ namespace Class_Library
             }
             return deck;
         }
-        public List<ICard> Deal()
+        public List<ICard> Deal(int dealAmount)
         {
             List<ICard> deal = new List<ICard>();
-            if (_cards.Count >= 2)
+            if (_cards.Count >= dealAmount)
             {
-                deal.Add(_cards[0]);
-                deal.Add(_cards[1]);
-                _cards.RemoveRange(0, 2);
+                if (dealAmount > 1)
+                {
+                    deal.Add(_cards[0]);
+                    deal.Add(_cards[1]);
+                    _cards.RemoveRange(0, 2);
+                }
+                else
+                {
+                    deal.Add(_cards[0]);
+                    _cards.RemoveRange(0, 1);
+                }
+
             }
             else
             {
                 _cards = FreshDeck();
                 Shuffle();
+                Console.WriteLine(" Shuffling Deck");
+                Thread.Sleep(1000);
             }
 
             return deal;
         }
+
         public void Shuffle()
         {
             Random rando = new Random();
@@ -54,10 +67,8 @@ namespace Class_Library
                 ICard card = _cards[j];
                 _cards[j] = _cards[i];
                 _cards[i] = card;
-
-
             }
-           
+
         }
         public List<ICard> GetShuffledDeck()
         {
